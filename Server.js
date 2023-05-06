5eea9d6db29e2aa6b21a58d849b3a3139f8738fe
Avik Roy <somavik99@gmail.com>
2023-05-06 13:21:19 +0530

    main

--- Server.js
+++ Server.js
@@ -1,1 +1,21 @@
-console.log(":)hello");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.get("/", (req, res) => {
+  res.send("Hello Node Api");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://somavik99:Raavana100@myapi.fhab1ru.mongodb.net/NodeAPI?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected");
+    app.listen(8080, () => {
+      console.log("Node api is running on vite");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
